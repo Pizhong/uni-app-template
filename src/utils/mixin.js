b233e7ef3b52e0e34242cc4d883638291c1bf2fd
sunduping <sunduping@aipai.com>
2022-05-27 17:40:24 +0800

    删除部分无用配置文件

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -1,13 +1,9 @@
 /*
  * @file: 全局混入
  */
-import $store from '../store'
+// import $store from '../store'
 
 export default {
   computed: {
-    // 1-货主 2-船东
-    $role() {
-      return $store.state.login.loginData.type
-    }
   }
 }
